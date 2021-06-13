088357fdd2d438ada5daaec30f4c2c76640d5eae
Alin Grecu <alin.s.grecu@gmail.com>
2021-06-13 17:06:43 +0300

    Lower max pixel to 50

--- core/constants.py
+++ core/constants.py
@@ -11,7 +11,7 @@ class Email:
 
 
 class Graph:
-    PLOT_MAX_PIXELS = 100  # Max plot height you want. This will be the height of max(data).
+    PLOT_MAX_PIXELS = 50  # Max plot height you want. This will be the height of max(data).
 
 
 class Simfonia:
